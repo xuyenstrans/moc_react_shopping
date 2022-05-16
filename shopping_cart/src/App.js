bfd10de17b3cee54296aa07142615a97ec5d90c1
Xuyên Trần <61092839+xuyenstrans@users.noreply.github.com>
2022-05-17 00:01:11 +0700

    Commit First

---
+++ shopping_cart/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className={'grid_container'}>
+            <header>
+                Header
+            </header>
+            <main>
+                Main
+            </main>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    );
+}
+
+export default App;
